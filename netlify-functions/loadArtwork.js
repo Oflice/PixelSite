16c9678fdcfd3b9be081c9d2ed7438a72b69fb4e
Oflice <ofliceofficial@gmail.com>
2024-12-07 13:41:06 +0200

    Update loadArtwork.js

--- netlify-functions/loadArtwork.js
+++ netlify-functions/loadArtwork.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async () => {
-    const filePath = path.join(__dirname, 'artwork.json');
+    const filePath = path.join(__dirname, '..', 'data', 'artwork.json');
 
     try {
         const data = fs.readFileSync(filePath, 'utf8');
         return { statusCode: 200, body: data };
     } catch (err) {
-        return { statusCode: 500, body: 'Error loading artwork' };
+        return { statusCode: 500, body: 'Failed to load artwork' };
     }
 };
